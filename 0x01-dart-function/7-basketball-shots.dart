31d7ccd6b53eecab8dcd5441386bc78aef1fdc3b
Ruslan Hamidov <124673207+RuslanHamidov@users.noreply.github.com>
2024-10-04 23:57:15 +0400

    Update 7-basketball-shots.dart

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,20 +1,7 @@
-num getPoints(Map team) {
-  num points = 0;
-  team.forEach((key, value) {
-    if (key == 'Free throws') {
-      points += value;
-    } else if (key == '2 pointer') {
-      points += value * 2;
-    } else if (key == '3 pointer') {
-      points += value * 3;
-    }
-  });
-  return points;
-}
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+   int teamAPoints = (teamA['Free throws'] ?? 0) + (teamA['2 pointers'] ?? 0) * 2 + (teamA['3 pointers'] ?? 0) * 3;
+  int teamBPoints = (teamB['Free throws'] ?? 0) + (teamB['2 pointers'] ?? 0) * 2 + (teamB['3 pointers'] ?? 0) * 3;
 
-num whoWins(Map teamA, Map teamB) {
-  num teamAPoints = getPoints(teamA);
-  num teamBPoints = getPoints(teamB);
   if (teamAPoints > teamBPoints) {
     return 1;
   } else if (teamAPoints < teamBPoints) {
