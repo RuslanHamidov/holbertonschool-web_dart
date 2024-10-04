168e315df57ece3ce8880c47d9e146946f58e998
Ruslan Hamidov <124673207+RuslanHamidov@users.noreply.github.com>
2024-10-05 00:07:29 +0400

    Update rick_and_morty.dart

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -3,27 +3,17 @@ import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
   try {
-    var response = await http.get(Uri.parse('https://rickandmortyapi.com/api/character'));
-
+    var response =
+        await http.get(Uri.parse('https://rickandmortyapi.com/api/character/'));
     if (response.statusCode == 200) {
-      // Decode the JSON response body
-      var data = jsonDecode(response.body);
-
-      // Extract characters from the data
-      List<dynamic> characters = data['results'];
-
-      // Print character names
-      characters.forEach((character) {
+      var characters = json.decode(response.body)['results'];
+      for (var character in characters) {
         print(character['name']);
-      });
+      }
     } else {
-      print('Request failed with status: ${response.statusCode}.');
+      throw Exception('Failed to fetch data');
     }
-  } catch (e) {
-    print('Failed to load data: $e');
+  } catch (err) {
+    print('error caught: $err');
   }
-}
-
-void main() async {
-  await printRmCharacters();
 }
